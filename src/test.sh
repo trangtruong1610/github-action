139a7e28f13757cb64404746f487acb39a47366a
Trang Truong <trang.truong@gigacover.com>
2020-05-17 15:29:01 +0700

    test run sh file

---
+++ src/test.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+s=$BASH_SOURCE ; s=$(dirname "$s") ; s=$(cd "$s" && pwd) ; SCRIPT_HOME="$s"  # get SCRIPT_HOME=executed script's path, containing folder, cd & pwd to get container path
+a="$SCRIPT_HOME/../../.."; a=$(cd "$a" && pwd); APP_HOME=$a; ROOT="$APP_HOME/../../.."; ROOT=$(cd "$ROOT" && pwd)
+
+echo 'Hello World'
+
