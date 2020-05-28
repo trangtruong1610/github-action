75b2f8576722522d8ead9699afe9edc31cb8faa7
Trang Truong <trang.truong@gigacover.com>
2020-05-28 10:00:49 +0700

    test new slack api

--- src/test.sh
+++ src/test.sh
@@ -2,4 +2,4 @@
 s=$BASH_SOURCE ; s=$(dirname "$s") ; s=$(cd "$s" && pwd) ; SCRIPT_HOME="$s"  # get SCRIPT_HOME=executed script's path, containing folder, cd & pwd to get container path
 a="$SCRIPT_HOME/../../.."; a=$(cd "$a" && pwd); APP_HOME=$a; ROOT="$APP_HOME/../../.."; ROOT=$(cd "$ROOT" && pwd)
 
-echo 'Run script success 6'
+echo 'Run script success 7'
